f767105a1def93c723ad8e91a8d58e6924f56df0
yassin-ajanif <y.ajanif@enjazdev.com>
2026-07-12 14:11:15 +0200

    Add TotalTtc column to DevisClient table in database
    - Created a new migration to add the TotalTtc column of type decimal with precision 18 and scale 2 to the DevisClient table.
    - Updated the AppDbContextModelSnapshot to reflect the new column in the database schema.
    - Implemented the Up and Down methods for the migration to handle adding and removing the column as needed.

---
+++ src/CoperativeTelouet.DataAccess/Migrations/20260712120929_AddDevisClientTotalTtc.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CoperativeTelouet.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDevisClientTotalTtc : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "TotalTtc",
+                table: "DevisClient",
+                type: "TEXT",
+                precision: 18,
+                scale: 2,
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TotalTtc",
+                table: "DevisClient");
+        }
+    }
+}
